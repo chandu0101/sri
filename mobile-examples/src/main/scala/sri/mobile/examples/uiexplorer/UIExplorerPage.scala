9685c3ae16951bab8f1c06a911d46ceec4b8e302
chandu0101 <chandu.csu2010@gmail.com>
2015-12-16 04:45:34 +0530

    fix loading indicator

--- mobile-examples/src/main/scala/sri/mobile/examples/uiexplorer/UIExplorerPage.scala
+++ mobile-examples/src/main/scala/sri/mobile/examples/uiexplorer/UIExplorerPage.scala
@@ -3,6 +3,7 @@ package sri.mobile.examples.uiexplorer
 import sri.core.ElementFactory._
 import sri.core.{ReactComponent, _}
 import sri.mobile.ReactNative
+import sri.mobile.examples.Spinner
 import sri.universal.components._
 import sri.universal.styles.UniversalStyleSheet
 
@@ -20,7 +21,7 @@ object UIExplorerPage {
     initialState(State())
 
     def render() = {
-      if (state.isLoading) Text()("Loading")
+      if (state.isLoading) View(style = styles.spinner)(Spinner())
       else ScrollView(style = UniversalStyleSheet.wholeContainer, keyboardShouldPersistTaps = true,
         keyboardDismissMode = keyboardDismissMode.INTERACTIVE)(
           children
@@ -32,6 +33,12 @@ object UIExplorerPage {
         setState(state.copy(isLoading = false))
       })
     }
+  }
+
+  object styles extends UniversalStyleSheet {
+    val spinner = style(justifyContent.center,
+    alignItems.center,
+    marginTop := 40)
   }
 
   val ctor = getTypedConstructor(js.constructorOf[Component], classOf[Component])
